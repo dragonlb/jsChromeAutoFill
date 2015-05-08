bab2d2873e7ff4f11d02aeac02b2e1b33e6beb41
Lb <dragonlib@163.com>
2015-05-08 11:31:40 +0800

    firt init
    init

---
+++ js/background.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    initActions();
+});
+
+function initActions() {
+    $("#on")[0].addEventListener("click", function (e) {
+        chrome.tabs.executeScript(null, {code: "switchLight('" + e.target.id + "');", allFrames: true});
+    });
+    $("#off")[0].addEventListener("click", function (e) {
+        chrome.tabs.executeScript(null, {code: "switchLight('" + e.target.id + "');", allFrames: true});
+    });
+    $("#_autoComplete")[0].addEventListener("click", function (e) {
+        chrome.tabs.executeScript(null, {code: "autoDeal();", allFrames: true});
+    });
+    $("#_tsHello")[0].addEventListener("click", function (e) {
+        chrome.tabs.executeScript(null, {code: "hello('123');", allFrames: true});
+    });
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    initialize(tabId);
+});
+
+chrome.tabs.onSelectionChanged.addListener(function (tabId, selectInfo) {
+    initialize(tabId);
+});
+
+function initialize(tabId) {
+    chrome.tabs.executeScript(tabId, {file: "js/jquery-1.9.1.min.js", allFrames: true});
+    chrome.tabs.executeScript(tabId, {file: "js/lbmars.js", allFrames: true});
+}
+
+chrome.extension.onRequest.addListener(
+    function (request, sender, sendResponse) {
+        chrome.tabs.executeScript(null, {code: "switchLight(" + request + ");", allFrames: true});
+    }
+);
---
+++ js/lbmars.js
@@ -0,0 +1,45 @@
+$('body').on("click", function(){
+		console.log("Hello Lb.");
+});
+function hello(name){
+	var vName = "";
+	if(name)	vName = name;
+	console.log("Do you know me?\t"+vName);
+}
+function switchLight(lightAction){
+    if(lightAction == 'off'){
+        document.body.style.backgroundColor='#000';
+    } else {
+        document.body.style.backgroundColor='#fff';
+    }
+}
+function autoDeal(){
+	var cnNames = ["张三","李四","王二","钱五","张三一","李四二","王二二","钱五二","张三三","李四三","王二三","钱五三"];
+	var enNames1 = ["zhan","li","wang","qian","zhan2","li2","wang2","qian2","zhan3","li3","wang3","qian3"];
+	var enNames2 = ["san","si","er","wu","san2","si2","er2","wu2","san3","si3","er3","wu3"];
+	$("[type_name='mobile']").each(function(idx, item){
+	  $(item).val(13524644444+idx/10);
+	});
+	$("[type_name='email']").each(function(idx, item){
+	  $(item).val("123@123.com");
+	});
+	$("[name*='fullName']").each(function(idx, item){
+	  $(item).val(cnNames[idx/cnNames.size()]);
+	});
+	$("[name*='firstName']").each(function(idx, item){
+	  $(item).val(enNames1[idx/enNames1.size()]);
+	});
+	$("[name*='lastName']").each(function(idx, item){
+	  $(item).val(enNames2[idx/enNames2.size()]);
+	});
+	$("[name$='idNo']").each(function(idx, item){
+	  $(item).val(342401198012121212+idx/10);
+	});
+	$("[name*='address']").each(function(idx, item){
+	  $(item).val("测试地址___"+idx);
+	});
+	$("[name*='postcode']").each(function(idx, item){
+	  $(item).val(201510+idx/10);
+	});
+	$("#user_name").val(cnNames[0]);
+}
